de2683f305d209823ecdcb55bcc785471d8e82b5
bearl <125728897+bearl27@users.noreply.github.com>
2025-06-21 16:35:22 +0900

    fix: スクレイピング対象のURLの一意性制約を削除

--- back/internal/companies/domain/entity.go
+++ back/internal/companies/domain/entity.go
@@ -9,7 +9,7 @@ type Company struct {
 	Description     string    `gorm:"not null" json:"description"`              // 企業の説明
 	Img             string    `gorm:"not null" json:"image"`                    // 企業のロゴ画像URL
 	Industry        string    `gorm:"not null" json:"industry"`                 // 業種（例: IT、製造、サービスなど）
-	ScrapeTargetURL string    `gorm:"not null;unique" json:"scrape_target_url"` // スクレイピング対象のURL
+	ScrapeTargetURL string    `gorm:"not null" json:"scrape_target_url"` // スクレイピング対象のURL
 	LastScrapeTime  time.Time `gorm:"not null" json:"last_scrape_time"`         // 最後にスクレイピングを行った日時
 	CreatedAt       time.Time `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt       time.Time `gorm:"autoUpdateTime" json:"updated_at"`
